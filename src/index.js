6e687b7a88c640acdc6b3717ea85ed0fd6f0f7a1
Ivanov.I <ivanov.ivan.zhivkov@gmail.com>
2023-05-24 17:20:21 +0300

    add cookie-parser and jsonwebtoken

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 
 const routes = require('./routes');
@@ -13,6 +14,7 @@ setupViewEngine(server);
 
 
 server.use(express.static('./src/public'));
+server.use(cookieParser());
 server.use(express.urlencoded({extended: false}));
 server.use(routes)
 
